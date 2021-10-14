dbf518cb26e92cb93d1dfb5bfc30fcabad563d7c
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-14 15:10:18 +0530

    formik ErrorMessage Component

--- src/Components/FormikComp.js
+++ src/Components/FormikComp.js
@@ -1,64 +1,69 @@
 import React from 'react'
-import {Formik, Form, Field} from 'formik'
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage
+} from 'formik'
 import * as Yup from 'yup'
 
+
 const initialValues = {
-  
+    name:'',
+    email:'',
+    channel:'',
 }
+
 const onSubmit = values =>{
-    
+    console.log('Form Data', values)
 }
+
 
 const validationSchema = Yup.object({
-    
+    name: Yup.string().required('Required!'),
+    email: Yup.string().email('Invalid email formate')
+    .required('Required!'),
+    channel: Yup.string().required('Required!')
 })
 
+
 function FormikComp() {
+  
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+     >
+          <Form>
+            <div className='form-control'>
+              <label htmlFor='name'>Name</label>
+              <Field type='text' id='name' name='name' />
+              <ErrorMessage name='name' />
+            </div>
 
-    console.log('visited fields', formik.touched)
-    return (
-        <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-        >
-        <Form>
-        <div className="form-control">
-            <label htmlFor="name">Name</label>
-            <Field 
-                type="text" 
-                id="name"
-                name="name" 
-                
-            />
-            {formik.touched.name && formik.errors.name ? <div className="error"> {formik.errors.name} </div>: null}
-         </div>
+            <div className='form-control'>
+              <label htmlFor='email'>Email</label>
+              <Field type='email' id='email' name='email' />
+              <ErrorMessage name='email'/>
 
-        <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <Field 
-                type="text" 
-                id="email"
-                name="email" 
-               
-            />
-            {formik.touched.email && formik.errors.email ? <div className="error"> {formik.errors.email} </div>: null}
-        </div>
+            </div>
+
+            <div className='form-control'>
+              <label htmlFor='channel'>Channel</label>
+              <Field
+                type='text'
+                id='channel'
+                name='channel'
+              />
+              <ErrorMessage name='channel' />
+            </div>
+            <button type='submit'>Submit</button>
+        </Form>
         
-        <div className="form-control">
-            <label htmlFor="channel">Channel</label>
-            <Field 
-                type="text" 
-                id="channel" 
-                name="channel" 
-               
-            />
-            {formik.touched.channel && formik.errors.channel ? <div className="error"> {formik.errors.channel} </div>: null}
-        </div>
-        <br/>
-        <button type="submit">Submit</button>
-        </Form> 
-        </Formik>
-    )
+      
+    </Formik>
+  )
 }
+
 export default FormikComp
