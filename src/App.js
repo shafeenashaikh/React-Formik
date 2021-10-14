ac1b121b9a652de9afb4fc0dc2c123e0a0919132
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-14 12:36:54 +0530

    form validation using yup library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FormValidation from './Components/FormValidation';
 import YoutubForm from './Components/YoutubForm';
 
 function App() {
   return (
     <div className="App">
-      <YoutubForm/>
+      {/* <YoutubForm/> */}
+      <FormValidation/>
     </div>
   );
 }
