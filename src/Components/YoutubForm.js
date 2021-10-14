89a2d769fec28233e4942d1051ce68debc3178db
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-14 10:19:51 +0530

    maneged form state using formik

--- src/Components/YoutubForm.js
+++ src/Components/YoutubForm.js
@@ -3,17 +3,43 @@ import {useFormik} from 'formik'
 
 function YoutubForm() {
 
-    const formik = useFormik({})
+    const formik = useFormik({
+        initialValues: {
+            name:'',
+            email:'',
+            channel:'',
+        }
+    })
+
+    console.log('Formik Values', formik.values)
     return (
         <div>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name"/>
+        <input 
+            type="text" 
+            id="name"
+            name="name" 
+            onChange={formik.handleChange} 
+            value={formik.values.name}
+         />
 
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email"/>
+        <input 
+            type="text" 
+            id="email"
+            name="email" 
+            onChange={formik.handleChange} 
+            value={formik.values.email}
+         />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel"/>
+        <input 
+            type="text" 
+            id="channel" 
+            name="channel" 
+            onChange={formik.handleChange} 
+            value={formik.values.channel}
+        />
         <br/>
         <button>Submit</button>
         </div>
