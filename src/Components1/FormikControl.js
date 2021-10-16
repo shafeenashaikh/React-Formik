11d9990343fa8de10d0b40d6fcffb9654d555ec9
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-10-16 13:04:01 +0530

    created RadioButtons component for selecting option uisng FormikControl components

--- src/Components1/FormikControl.js
+++ src/Components1/FormikControl.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Input from './Input'
+import RadioButtons from './RadioButtons'
 import Select from './Select'
 import TextArea from './TextArea'
 
@@ -13,6 +14,7 @@ import TextArea from './TextArea'
         case 'select':
             return <Select {...rest}/>
         case 'radio':
+            return <RadioButtons {...rest}/>
         case 'checkbox':
         case 'date':
         default: return null
--- src/Components1/FormikContsiner.js
+++ src/Components1/FormikContsiner.js
@@ -3,22 +3,33 @@ import {Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import FormikControl from './FormikControl'
 
+
 function FormikContainer() {
+
     const dropdownOptions = [
         { key: 'Select an option', value: ''},
         { key: 'option 1', value: 'option1'},
         { key: 'option 2', value: 'option2'},
         { key: 'option 3', value: 'option3'}
     ]
+
+    const radioOptions = [
+        {key: 'Option 1', value: 'rOption1'},
+        {key: 'Option 2', value: 'rOption2'},
+        {key: 'Option 3', value: 'rOption3'},
+    ]
+
     const initialValues = {
         email:'',
         description:'',
-        selectOption:''
+        selectOption:'',
+        radioOption:''
     }
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
         description: Yup.string().required('Required'),
-        selectOption: Yup.string().required('Required')
+        selectOption: Yup.string().required('Required'),
+        radioOption: Yup.string().required('Required')
     })
     const onSubmit = values => console.log('Form data', values)
     return (
@@ -46,6 +57,12 @@ function FormikContainer() {
                         label='Select a topic'
                         name='selectOption'
                         options={dropdownOptions}
+                       />
+                       <FormikControl
+                        control='radio'
+                        label= 'Radio Topic'
+                        name= 'radioOption'
+                        options= {radioOptions}
                        />
                        <button type='submit'>Submit</button>
                    </Form>
